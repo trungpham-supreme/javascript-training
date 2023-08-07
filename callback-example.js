103f5e29074f5e77dd453485f51c757387e0e4d1
TrungPham126 <trungpt@tekup.vn>
2023-08-07 11:21:30 +0700

    feat: add callback example

---
+++ callback-example.js
@@ -0,0 +1,33 @@
+// A callback is a function passed as an argument to another function
+// JavaScript solves this problem using callbacks.
+// A callback function can run after another function has finished
+
+// example 1
+function sayHi() {
+  console.log("Hello");
+}
+
+function meetUp(callback) {
+  callback();
+}
+
+// meetUp(sayHi);
+
+// example 2
+function fetchData(callback) {
+  setTimeout(() => {
+    const data = { message: "Done!" };
+    callback(null, data);
+  }, 1000);
+}
+
+function processData(err, data) {
+  if (err) {
+    console.error("Error:", err);
+  } else {
+    console.log("Data:", data);
+  }
+}
+
+fetchData(processData);
+
