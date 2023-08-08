5ab82cc7a64f232d0d07b9220484293db20bff7b
TrungPham126 <trungpt@tekup.vn>
2023-08-08 16:46:11 +0700

    feat: add exercise-2-4

---
+++ exercise-2/exercise-2-4/exercise-2-4.js
@@ -0,0 +1,12 @@
+function getAttributes(){
+  let result = document.getElementById('w3r');
+  alert(
+    `
+    type: ${result.type}
+    hreflang: ${result.hreflang}
+    rel: ${result.rel}
+    target: ${result.target}
+    href: ${result.href}
+    `
+  )
+}
