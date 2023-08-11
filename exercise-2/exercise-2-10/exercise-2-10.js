ffcd84a2bfeec5efdf2b2272befda9bce11e15e1
TrungPham126 <trungpt@tekup.vn>
2023-08-11 11:01:00 +0700

    fix: exercise-2-10 add type number input radius and readonly input volume

--- exercise-2/exercise-2-10/exercise-2-10.js
+++ exercise-2/exercise-2-10/exercise-2-10.js
@@ -1,15 +1,16 @@
 function volumeSphere() {
   let volume; // Define volume
   let radius = document.getElementById('radius').value; // Define radius
-  // Set attribute type number for input radius
-  document.getElementById('radius').setAttribute('type', 'number');
   // Calulate volume
   volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
   volume = volume.toFixed(2);
-  document.getElementById('volume').readOnly = true;
   document.getElementById('volume').value = volume;
   // Prevent form submission
   return false;
+}
+
+function finishTask(){
+
 }
 
 document.getElementById('MyForm').onsubmit = volumeSphere;
