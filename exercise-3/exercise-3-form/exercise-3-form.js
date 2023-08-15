a1ee637b1e21b43e5b63ad847a94a4fe00bfa548
TrungPham126 <trungpt@tekup.vn>
2023-08-15 15:02:33 +0700

    feature: exercise-3-form display user information on UI

--- exercise-3/exercise-3-form/exercise-3-form.js
+++ exercise-3/exercise-3-form/exercise-3-form.js
@@ -3,52 +3,81 @@ const signUpForm = document.querySelector('.signup-form');
 function validateSignUpForm(event) {
   event.preventDefault();
 
+  // Information from input
   const email = document.querySelector('#email').value;
-  const userName = document.querySelector('#userName').value;
+  const name = document.querySelector('#name').value;
   const password = document.querySelector('#password').value;
   const confirmPassword = document.querySelector('#confirmPassword').value;
+
   const textRegex = /^[a-zA-Z0-9\\,\\.\s]/g;
   const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
   const passwordRegex = /[^\w]/;
+  const userInfo = document.querySelector('.user-info');
+  userInfo.style.display = 'none';
+  let isValid = true;
 
   clearError();
 
-  validateEmail(email, emailRegex);
-  validateUserName(userName, textRegex);
-  validatePassword(password, confirmPassword, passwordRegex);
+  // Validate data from input
+  isValid = validateEmail(email, emailRegex);
+  isValid = validateUserName(name, textRegex);
+  isValid = validatePassword(password, confirmPassword, passwordRegex);
+  isValid = validateConfirmPassword(password, confirmPassword, passwordRegex);
+
+  if (isValid) {
+    // Display user info if all input is valid
+    document.getElementById('userName').textContent = name;
+    document.getElementById('userEmail').textContent = email;
+    document.getElementById('userPassword').textContent = password;
+    document.getElementById('userConfirmPassword').textContent =
+      confirmPassword;
+    userInfo.style.display = 'block';
+  }
 }
 
+// Validate name input
 function validateUserName(name, regex) {
   if (!name.match(regex) || name.trim() !== name || !name) {
     displayError(
       'userNameError',
       'Please enter the correct format for Username. (No leading or trailing spaces)'
     );
+    return false;
   }
+  return true;
 }
 
+// Validate email input
 function validateEmail(email, regex) {
   if (!email.match(regex)) {
     displayError(
       'emailError',
       'Email address empty or wrong format. Example: abc@g.com'
     );
+    return false;
   }
+  return true;
 }
 
+// Validate password input
 function validatePassword(password, confirmPassword, regex) {
   if (password.length < 8 || !regex.test(password)) {
     displayError(
       'passwordError',
       'Please enter the correct format for password. (8 charaters at least one non-letter)'
     );
+    return false;
   }
+}
 
+// Validate confirmPassword input
+function validateConfirmPassword(password, confirmPassword, regex) {
   if (confirmPassword.length < 8 || !regex.test(confirmPassword)) {
     displayError(
       'confirmPasswordError',
       'Please enter the correct format for confirm password. (8 charaters at least one non-letter)'
     );
+    return false;
   }
 
   if (confirmPassword !== password) {
@@ -56,13 +85,17 @@ function validatePassword(password, confirmPassword, regex) {
       'confirmPasswordError',
       'Make sure password and confirm password match'
     );
+    return false;
   }
+  return true;
 }
 
+// Display error
 function displayError(error, errorMessage) {
   document.getElementById(error).innerHTML = errorMessage;
 }
 
+// Clear error
 function clearError() {
   let errors = document.querySelectorAll('.error');
   errors.forEach((err) => {
