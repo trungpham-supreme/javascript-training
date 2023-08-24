ba2d33439cb1c0c89d5abea0d8d29bcc1cb7ff8a
TrungPham126 <trungpt@tekup.vn>
2023-08-24 16:18:01 +0700

    feat: exercise-shopping-cart display list product

---
+++ exercise-shopping-cart/models/product-model.js
@@ -0,0 +1,20 @@
+import Observable from './observable.js';
+
+class ProductModel extends Observable {
+  constructor() {
+    super();
+    this.products = [
+      { id: 0, name: 'coffee', price: 1000, quantity: 5 },
+      { id: 1, name: 'boba tea', price: 1500, quantity: 7 },
+      { id: 2, name: 'milk tea', price: 1200, quantity: 10 },
+      { id: 3, name: 'smoothie', price: 2000, quantity: 4 },
+      { id: 4, name: 'matcha', price: 1000, quantity: 5 },
+    ];
+  }
+
+  getProducts() {
+    return this.products;
+  }
+}
+
+export { ProductModel };
