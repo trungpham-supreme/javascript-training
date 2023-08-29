098eb51c8dc6db82d51fb39173cfd49e848d50b4
TrungPham126 <trungpt@tekup.vn>
2023-08-29 17:14:35 +0700

    fix: exercise-shopping-cart display products page

--- exercise-shopping-cart/src/controllers/product-controller.js
+++ exercise-shopping-cart/src/controllers/product-controller.js
@@ -2,24 +2,6 @@ class ProductController {
   constructor(model) {
     this.model = model;
   }
-
-  // Event listener interface
-  handleEvent(event) {
-    event.stopPropagation();
-    switch (event.type) {
-      case 'click':
-        this.clickHandler(event.target);
-        break;
-      default:
-        console.log(event.target);
-    }
-  }
-
-  // Change the model
-  clickHandler(target) {
-    this.model.decreaseProduct(target.dataset.id);
-    this.model.notify(this.model);
-  }
 }
 
 export { ProductController };
--- exercise-shopping-cart/src/models/product-model.js
+++ exercise-shopping-cart/src/models/product-model.js
@@ -3,28 +3,6 @@ import Observable from './observable.js';
 class ProductModel extends Observable {
   constructor() {
     super();
-    this.products = this.getProducts();
-  }
-
-  decreaseProduct(index) {
-    const parsedIndex = parseInt(index);
-
-    if (
-      isNaN(parsedIndex) ||
-      parsedIndex < 0 ||
-      parsedIndex >= this.products.length
-    ) {
-      console.error('Invalid index:', index);
-      return;
-    }
-
-    const product = this.products[parsedIndex];
-    if (product.quantity > 0) {
-      product.quantity--;
-      localStorage.setItem('products', JSON.stringify(this.products));
-    } else {
-      alert('The product is out of stock!');
-    }
   }
 
   getProducts() {
